5bcc20fbe62d00e98a69dcc3c30ecf087f5f837e
sddlol <sdd12345654321@163.com>
2026-02-19 00:15:02 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# place any of your own overrides here.
-# see bombsquad_server for details on what you can override
-# examples (uncomment to use):
-config['partyName'] = '🤣Solo Duel💜 杭州单挑死斗😈 Happy 2025🎉🥳'
-# config['sessionType'] = 'teams'
-config['maxPartySize'] = 13
-config['port'] = 43211
-config['playlistCode'] = 485669
